ee38aabd4b5c1f93e4ed63eb7e4e92e2ddeb8d50
yose kornelius <yose.kornelius@ti.ukdw.ac.id>
2019-11-25 16:54:22 +0700

    push anggaran

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/AnggaranController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author YOSE
+ */
+public class AnggaranController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
